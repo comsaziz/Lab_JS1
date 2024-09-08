2170b0ad7b39330da24d2c3e4899bba1ee9439d5
comsaziz <comsaziz@gmail.com>
2024-09-08 12:32:21 +0300

    Add New Lab

---
+++ main.js
@@ -0,0 +1,58 @@
+
+
+
+// Q1:
+
+let Rectangle = 12 *3
+console.log(Rectangle );
+
+
+// Q2
+// Celsius to Fahrenheit
+let celsius = 37; 
+let feh = (celsius * 9/5) + 32 ;
+console.log(feh);
+
+let fahrenheit = 100;
+cel = (fahrenheit -32) * 5/9 ;
+console.log(cel)
+
+// Q3
+let hour = 5;
+let second = 3600 * hour
+console.log(second);
+// Q4
+let thingOne = "Hello";
+let thingTwo = "Word";
+console.log(thingOne + thingTwo);
+
+// Q5
+
+var Bill = 10.25 + 3.99 + 7.15
+var tip = Bill * 15/100;
+var total = Bill + tip;
+console.log(`Bill is ${Bill} tip is ${tip} total is ${total}`)
+
+
+// Q6
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+var madLib = "The Intro to JavaScript course is"+ adjective1+ "James and Julia are so"+adjective2+ "I cannot wait to work through the rest of this"+adjective3+  "content!"
+
+console.log(madLib);
+
+// Q7
+
+ var firstName = "Julia"
+ var interest = "cats"
+ var hobby =  "play video games"
+ var awesomeMessage = "Hi, my name is" + firstName+ "I love" +interest+ "In my spare time, I like to" +hobby+ "";
+ console.log(awesomeMessage);
+
+
+
+
+
+
+
